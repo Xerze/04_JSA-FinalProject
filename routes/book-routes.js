21fb9ac78ba89ebbe14401d70bfc6649c0686a2b
Xerze (M. Granados) <xerzewyth@gmail.com>
2022-09-28 20:48:02 -0500

    feat: creation of the verbs and methods for the api

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -12,10 +12,10 @@ const router = router();
 // Usamos el objeto router para crear rutas para los verbos de la API.
 // los métodos usan como parámetro una ruta y un handler (manejador).
 router.get('/', bookController.getBooks);           // Traducción:  GET http://localhost:6000/api-restfull/books/
-router.get('/:sku', bookController.getBook);        // Traducción:  GET http://localhost:6000/api-restfull/books/:sku
+router.get('/:id', bookController.getBook);         // Traducción:  GET http://localhost:6000/api-restfull/books/:id
 router.post('/', bookController.addBook);           // Traducción:  POST http://localhost:6000/api-restfull/books/
-router.put('/:sku', bookController.updateBook);     // Traducción:  PUT http://localhost:6000/api-restfull/books/:sku
-router.delete('/:sku', bookController.deleteBook);  // Traducción:  DELETE http://localhost:6000/api-restfull/books/:sku
+router.put('/:id', bookController.updateBook);      // Traducción:  PUT http://localhost:6000/api-restfull/books/:id
+router.delete('/:id', bookController.deleteBook);   // Traducción:  DELETE http://localhost:6000/api-restfull/books/:id
 
 // Exportamos las rutas para poder usarlas en otras partes del proyecto.
 export default router;
